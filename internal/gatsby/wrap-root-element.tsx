f1821c5109df2731a21e64fccf4edcd20616d6e0
jambottle <jaewon.walks@gmail.com>
2024-10-27 17:35:14 +0900

    chore(deps): update dependency @vercel/analytics to ^1.3.2

--- internal/gatsby/wrap-root-element.tsx
+++ internal/gatsby/wrap-root-element.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 
 import { CoilProvider } from "@alxshelepenok/diesel";
+import { Analytics } from "@vercel/analytics/react";
 import { WrapRootElementBrowserArgs } from "gatsby";
 
 const wrapRootElement = ({
   element,
 }: WrapRootElementBrowserArgs): React.ReactElement => (
-  <CoilProvider>{element}</CoilProvider>
+  <CoilProvider>
+    {element}
+    <Analytics />
+  </CoilProvider>
 );
 
 export { wrapRootElement };
